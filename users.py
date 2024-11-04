4aada58577411148a465ba8d0ae842f6b25b4e53
mldaill <daillmarianela@gmail.com>
2024-11-04 18:09:39 +0000

    Guardar cambios en users.py antes de fusionar

--- users.py
+++ users.py
@@ -3,6 +3,20 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 
+class UserIn(BaseModel):
+    name: str
+    email: str
+    password: int
+    is_admin: bool
+
+
+class UserOut(BaseModel):
+    id: int
+    name: str
+    email: str
+    is_admin: bool
+
+
 class User(BaseModel):
     id: int
     name: str
@@ -11,7 +25,7 @@ class User(BaseModel):
     is_admin: bool
 
 
-users = [
+users: list[User] = [
     User(
         id=1,
         name="Jose Ortiz",
@@ -41,25 +55,28 @@ users = [
 
 router = APIRouter()
 
+last_id = 5
+
 
 @router.get("/users")
-def all_users():
+def list_all_users():
     return users
 
 
 @router.post("/users")
-def new_user(user: User):
-    for u in users:
-        if u.id == user.id:
-            return "El usuario ya existe"
+def create_new_user(user_in: UserIn):
+    global last_id
+    last_id = last_id + 1
+    user = User(id=last_id, **user_in.model_dump())
     users.append(user)
-    return "Usuario creado con éxito"
+    user_out = UserOut(**user.model_dump())
+    return user_out
 
 
-@router.delete("/users/{id}")
-def delete_user(id: int):
+@router.delete("/users/{user_id}")
+def delete_user(user_id: int):
     for u in users:
-        if u.id == id:
+        if u.id == user_id:
             users.remove(u)
             return "Usuario eliminado"
     return "Usuario no encontrado"
@@ -77,9 +94,9 @@ def update_user(user: User):
     return "Usuario no encontrado"
 
 
-@router.get("/users/search")
-def search_user(id: int):
+@router.get("/users/{user_id}")
+def search_user(user_id: int):
     for u in users:
-        if u.id == id:
-            return u
+        if u.id == user_id:
+            return UserOut(**u.model_dump())
     return "Usuario no encontrado"
