b7cd55e9486921eda420ac3f034e3a21ed6bedec
mldaill <daillmarianela@gmail.com>
2024-11-07 18:55:03 +0000

    actualización de books

--- books.py
+++ books.py
@@ -1,10 +1,10 @@
-from typing import Union
-from fastapi import APIRouter
+from typing import Annotated, Protocol, Union
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
 
 class Book(BaseModel):
-    id: int
+    isbn: str
     title: str
     author: str
     published_year: int
@@ -13,28 +13,28 @@ class Book(BaseModel):
 
 books = [
     Book(
-        id=1,
+        isbn="9788478887194",
         title="El Principito",
         author="Antoine de Saint-Exupéry",
         published_year=1943,
         category_id="Novela",
     ),
     Book(
-        id=2,
+        isbn="9781400000111",
         title="Álamos Talados",
         author="Julio Cortázar",
         published_year=1953,
         category_id="Novela",
     ),
     Book(
-        id=3,
+        isbn="9788497403201",
         title="Marianela",
         author="Benito Pérez Galdós",
         published_year=1878,
         category_id="Novela",
     ),
     Book(
-        id=4,
+        isbn="9788490709935",
         title="El Club de las 5 de la Mañana",
         author="Robin Sharma",
         published_year=2018,
@@ -43,47 +43,61 @@ books = [
 ]
 
 
+class BookRepository(Protocol):
+    def list(self): ...
+
+    def save(self, book: Book): ...
+
+    def get(self, isbn: str) -> Book | None: ...
+
+    def delete(self, isbn: str): ...
+
+
+class RamBookRepository(BookRepository):
+    def __init__(self, books: list[Book]):
+        self.books = books
+
+    def list(self) -> list[Book]:
+        return self.books
+
+    def save(self, book: Book):
+        for b in self.books:
+            if b.isbn == book.isbn:
+                self.books.remove(b)
+        self.books.append(book)
+
+    def get(self, isbn: str) -> Book | None:
+        for b in self.books:
+            if b.isbn == isbn:
+                return b
+            return None
+
+    def delete(self, isbn: str):
+        for b in self.books:
+            if b.isbn == isbn:
+                self.books.remove(b)
+
+
 router = APIRouter()
+repo = RamBookRepository(books)
 
 
 @router.get("/books")
-def all_books():
-    return books
-
-
-@router.post("/books")
-def new_book(book: Book):
-    for b in books:
-        if b.title == book.title:
-            return "El libro ya existe"
-    books.append(book)
-    return "Libro creado"
-
-
-@router.delete("/books/{title}")
-def delete_book(title: str):
-    for t in books:
-        if t.title == title:
-            books.remove(t)
-            return "Libro eliminado"
-    return "Libro no encontrado"
+def list_books(repo: Annotated[BookRepository, Depends(lambda: repo)]):
+    return repo.list()
 
 
 @router.put("/books")
-def update_book(book: Book):
-    for b in books:
-        if b.title == book.title:
-            b.id = book.id
-            b.author = book.author
-            b.published_year = book.published_year
-            b.category_id = book.category_id
-            return "Libro actualizado"
-    return "Libro no encontrado"
+def insert_book(book: Book, repo: Annotated[BookRepository, Depends(lambda: repo)]):
+    repo.save(book)
+    return "Libro creado"
 
 
-@router.get("/books/search")
-def search_book(title: str):
-    for t in books:
-        if t.title == title:
-            return t
-    return "Libro no encontrado"
+@router.delete("/books/{isbn}")
+def delete_book(isbn: str, repo: Annotated[BookRepository, Depends(lambda: repo)]):
+    repo.delete(isbn)
+
+
+@router.get("/books/{isbn}")
+def get_book(isbn: str, repo: Annotated[BookRepository, Depends(lambda: repo)]):
+    return repo.get(isbn)
