7e245e61ac2caddc33135b905cccfa61f77d9140
mldaill <daillmarianela@gmail.com>
2024-11-19 13:53:50 +0000

    actualizacion docker, reservations e users

--- reservations.py
+++ reservations.py
@@ -1,13 +1,18 @@
-from typing import Union
+from typing import Annotated, Protocol, Union
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from books import Book, books
 from users import User, users
 from datetime import date
 
-router = APIRouter()
 
+class ReservationIn(BaseModel):
+    user_id: int
+    isbn: str
+    pickup_date: date
+    return_date: date
+docker-compose.yml
 
 class Reservation(BaseModel):
     id: int
@@ -17,14 +22,68 @@ class Reservation(BaseModel):
     return_date: date
 
 
-reservations: List[Reservation] = []
+class ReservationRepository(Protocol):
 
-last_reservation_id = 0
+    def list(self) -> List[Reservation]: ...
+
+    def create(self, user: User, book: Book) -> Reservation: ...
+
+    def save(self, reservation: Reservation): ...
+
+    def get(self, id: int) -> Union[Reservation, None]: ...
+
+    def delete(self, id: int): ...
+
+
+class RamReservationRepository(ReservationRepository):
+    def __init__(self, reservations: List[Reservation]):
+        self.reservations = reservations
+        self.last_reservation_id = len(reservations)
+
+    def list(self) -> List[Reservation]:
+        return self.reservations
+
+    def create(self, user: User, book: Book) -> Reservation:
+        self.last_reservation_id += 1
+        r = Reservation(
+            id=self.last_reservation_id,
+            book=book,
+            user=user,
+            pickup_date=date.today(),
+            return_date=date.today(),
+        )
+        self.reservations.append(r)
+        return r
+
+    def save(self, reservation: Reservation):
+        for r in self.reservations:
+            if r.isbn == reservation.isbn:
+                self.reservations.remove(r)
+        self.reservations.append(reservation)
+
+    def get(self, id: int) -> Reservation | None:
+        for r in self.reservations:
+            if r.id == id:
+                return r
+        return None
+
+    def delete(self, id: int):
+        for r in self.reservations:
+            if r.id == id:
+                self.reservations.remove(r)
+
+
+router = APIRouter()
+repo = RamReservationRepository([])
 
 
 @router.post("/reservations")
 def create_reservation(
-    user_id: int, book_id: int, pickup_date: date, return_date: date
+    user_id: int,
+    isbn: str,
+    pickup_date: date,
+    return_date: date,
+    repo: Annotated[ReservationRepository, Depends(lambda: repo)],
 ):
     user = None
     book = None
@@ -34,56 +93,50 @@ def create_reservation(
             user = u
             break
     for b in books:
-        if b.id == book_id:
+        if b.isbn == isbn:
             book = b
             break
     if user == None or book == None:
-        return " El user o el book no existe"
+        return "El user o el book no existe"
 
-    for r in reservations:
-        if r.book.id == book_id:
+    for r in repo.list():
+        if r.book.isbn == isbn:
             if (
                 r.pickup_date <= return_date
                 and r.return_date >= pickup_date
                 or r.return_date >= pickup_date
                 and r.pickup_date <= return_date
             ):
-                return " El libro ya se encuentra reservado"
+                return "El libro ya se encuentra reservado"
 
-    global last_reservation_id
-    last_reservation_id = last_reservation_id + 1
-    r = Reservation(
-        id=last_reservation_id,
+    r = ReservationIn(
         book=book,
         user=user,
         pickup_date=pickup_date,
         return_date=return_date,
     )
-    reservations.append(r)
-    return r
+    return repo.create(r)
 
 
 @router.get("/reservations")
 def user_reservations(user_id: int = None):
     if user_id == None:
-        return reservations
-    ret = []
-    for r in reservations:
-        if r.user.id == user_id:
-            ret.append(r)
-    return ret
+        return repo.list()
+    return [r for r in repo.list() if r.user.id == user_id]
 
 
 @router.delete("/reservations/{reservation_id}")
 def delete_reservations(reservation_id: int):
-    for r in reservations:
+    for r in repo.list():
         if r.id == reservation_id:
-            reservations.remove(r)
+            repo.list().remove(r)
             return "La reserva fue cancelada"
+    return "Reserva no encontrada"
 
 
-@router.get("/reservations/{reservations_id}")
+@router.get("/reservations/{reservation_id}")
 def details_reservations(reservation_id: int):
-    for r in reservations:
+    for r in repo.list():
         if r.id == reservation_id:
             return r
+    return "Reserva no encontrada"
--- users.py
+++ users.py
@@ -1,5 +1,5 @@
-from typing import Union
-from fastapi import APIRouter
+from typing import Annotated, Protocol, Union
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
 
@@ -53,50 +53,90 @@ users: list[User] = [
     ),
 ]
 
+
+class UserRepository(Protocol):
+    def list(self): ...
+
+    def create(self, user: UserIn) -> User: ...
+
+    def save(self, user: User): ...
+
+    def get(self, id: int) -> User | None: ...
+
+    def delete(self, id: int): ...
+
+
+class RamUserRepository(UserRepository):
+    def __init__(self, users: list[User]):
+        self.users = users
+        self.last_user_id = max(user.id for user in users) if users else 0
+
+    def list(self) -> list[User]:
+        return self.users
+
+    def create(self, user_in: UserIn) -> User:
+        new_id = self.last_user_id + 1
+        self.last_user_id = new_id
+
+        new_user = User(
+            id=new_id,
+            name=user_in.name,
+            email=user_in.email,
+            password=user_in.password,
+            is_admin=user_in.is_admin,
+        )
+
+        self.users.append(new_user)
+
+        return UserOut(
+            id=new_user.id,
+            name=new_user.name,
+            email=new_user.email,
+            is_admin=new_user.is_admin,
+        )
+
+    def save(self, user: User):
+        for u in self.users:
+            if u.id == user.id:
+                self.users.remove(u)
+        self.users.append(user)
+
+    def get(self, id: int) -> User | None:
+        for u in self.users:
+            if u.id == id:
+                return u
+            return None
+
+    def delete(self, id: int):
+        for u in self.users:
+            if u.id == id:
+                self.users.remove(u)
+        
+
+
 router = APIRouter()
+repo = RamUserRepository(users)
 
-last_id = 5
 
-
-@router.get("/users")
-def list_all_users():
-    return users
+@router.get("/users")docker-compose.yml
+def list_users(repo: Annotated[UserRepository, Depends(lambda: repo)]):
+    return repo.list()
 
 
 @router.post("/users")
-def create_new_user(user_in: UserIn):
-    global last_id
-    last_id = last_id + 1
-    user = User(id=last_id, **user_in.model_dump())
-    users.append(user)
-    user_out = UserOut(**user.model_dump())
-    return user_out
+def create_new_user(user_in: UserIn, repo: Annotated[UserRepository, Depends(lambda: repo)]):
+    return repo.create(user_in)
 
 
 @router.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    for u in users:
-        if u.id == user_id:
-            users.remove(u)
-            return "Usuario eliminado"
-    return "Usuario no encontrado"
+def delete_user(user_id: int, repo: Annotated[UserRepository, Depends(lambda: repo)]):
+    return repo.delete(user_id)
 
 
 @router.put("/users")
-def update_user(user: User):
-    for u in users:
-        if u.id == user.id:
-            u.name = user.name
-            u.email = user.email
-            u.password = user.password
-            u.is_admin = user.is_admin
-            return "Usuario actualizado"
-    return "Usuario no encontrado"
-
+def update_user(user: User , repo: Annotated[UserRepository, Depends(lambda: repo)]):
+    return repo.save(user)
 
 @router.get("/users/{user_id}")
-def search_user(user_id: int):
-    for u in users:
-        if u.id == user_id:
-            return UserOut(**u.model_dump())
-    return "Usuario no encontrado"
+def get_user(user_id: int, repo: Annotated[UserRepository, Depends(lambda: repo)]):
+    return repo.get(user_id)
